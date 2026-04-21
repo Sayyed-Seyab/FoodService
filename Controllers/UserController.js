c32973bbcf29baf6b8459eaecb0f4891417398ef
Sayyed-Seyab <syed.seyab767@gmail.com>
2026-04-21 22:33:24 +0300

    new chang

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,6 +1,6 @@
 
 import validator from "validator";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs"
 import jwt from "jsonwebtoken"
 import dotenv from "dotenv";
 import UserSchema from "../Models/UserSchema.js"
